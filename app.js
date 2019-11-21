5ca075a7a2da902005e33c79400640783a96fa37
apanchbhaya <57445630+apanchbhaya@users.noreply.github.com>
2019-11-21 14:07:37 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode = 'environment' }, audio: false };
+var constraints = { video: { facingMode = 'environment' },
+else {
+    videoConstraints.deviceId = { exact: select.value }
+
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
@@ -13,9 +16,9 @@ function cameraStart() {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
     })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
+    // .catch(function(error) {
+    //     console.error("Oops. Something is broken.", error);
+    // });
 }
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
