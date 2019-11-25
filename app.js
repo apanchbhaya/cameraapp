630db5f88b0925c4b8ecfaa8de69236dd9da77ad
apanchbhaya <57445630+apanchbhaya@users.noreply.github.com>
2019-11-25 01:08:10 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ function cameraStart() {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
     })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
+    
+
 }
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
